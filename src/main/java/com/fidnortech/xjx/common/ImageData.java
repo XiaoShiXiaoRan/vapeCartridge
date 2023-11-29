b997b526f053d7528c28f1fa16477354c3b9e31f
xjx <1749512834@qq.com>
2023-11-29 18:12:56 +0800

    富文本图片接口

--- src/main/java/com/fidnortech/xjx/common/ImageData.java
+++ src/main/java/com/fidnortech/xjx/common/ImageData.java
@@ -11,7 +11,7 @@ public class ImageData {
     String href;
     public ImageData(String name) {
         this.url = "http://192.168.0.160:62001/api/article/image/"+name;
-        this.alt = "image";
-        this.href = "";
+        this.alt = name;
+        this.href = "http://192.168.0.160:62001/api/article/image/"+name;
     }
 }
