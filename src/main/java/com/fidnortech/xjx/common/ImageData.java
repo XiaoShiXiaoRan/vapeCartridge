3c07840ed8cc418aaaeee31714e8b7e489d9733b
xjx <1749512834@qq.com>
2023-11-29 14:56:15 +0800

    富文本图片接口

---
+++ src/main/java/com/fidnortech/xjx/common/ImageData.java
@@ -0,0 +1,17 @@
+package com.fidnortech.xjx.common;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+
+@Data
+public class ImageData {
+
+    String url;
+    String alt;
+    String href;
+    public ImageData(String name) {
+        this.url = "http://192.168.0.160:62001/api/article/image/"+name;
+        this.alt = "image";
+        this.href = "";
+    }
+}
